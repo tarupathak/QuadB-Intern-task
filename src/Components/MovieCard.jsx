51251fdfb5f7add83e2a47d82ec374bbdf21468e
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-11 06:53:32 +0530

    Added first page

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Components.css";
+import axios from "axios";
 import cardimg from "../Images/MovieCard.jpg";
 
 const MovieCard = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://api.tvmaze.com/search/shows?q=all")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        alert("Something went wrong.");
+      });
+  }, []);
   return (
-    <div className="card">
-      <img src={cardimg} alt="card img" className="card-img" />
-      <div className="card-body">
-        <h1 className="card-title">XYZ</h1>
-        <p className="Card-sub-title">jsdushdb</p>
-        <p className="card-info">hdjhd</p>
-        <button classname="btn">Book</button>
+    <>
+    <div id="cards-design">
+      {data.map((item) => {
+        return (
+          <div className="card">
+            <img src={cardimg} alt="card img" className="card-img" />
+            <div className="card-body">
+              <h1 className="card-title">{item.show.name}</h1>
+              <p className="Card-sub-title"><div><span>Movie Score : </span><span>{item.score}</span></div>
+              <div><span>Movie Id : </span><span>{item.show.id}</span></div>
+              </p>
+              <p className="card-info">
+                <div><span>Type : </span><span>{item.show.type}</span></div>
+                <div><span>Language : </span><span>{item.show.language}</span></div>
+                <div><span>Genres : </span><span>{item.show.genres}</span></div>
+                <div><span>Status : </span><span>{item.show.status}</span></div>
+                <div><span>Runtime : </span><span>{item.show.runtime}</span></div>
+                <div><span>Average Runtime : </span><span>{item.show.averageRuntime}</span></div>
+                <div><span>Premierd : </span><span>{item.show.premiered}</span></div>
+                <div><span>Ended : </span><span>{item.show.ended}</span></div>
+                <div><span>Schedule : </span><span>{item.show.schedule.time} </span>on<span> {item.show.schedule.days}</span></div>
+                <div><span>Average Rating : </span><span>{item.show.rating.average}</span></div>
+              </p>
+              <button classname="btn">Book</button>
+            </div>
+          </div>
+        );
+      })}
       </div>
-    </div>
+    </>
   );
 };
 
